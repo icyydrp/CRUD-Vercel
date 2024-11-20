1618c48a170d0a76a5ba410077b5f02735157a57
icyydrp <93287905+icyydrp@users.noreply.github.com>
2024-11-19 20:27:09 -0500

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const db = mysql.createPool({
   host: process.env.DB_HOST || 'autorack.proxy.rlw.net', // El host de Railway
   user: process.env.DB_USER || 'root', // Usuario de la base de datos
-  password: process.env.DB_PASSWORD || '<tu-password>', // Reemplaza <tu-password> por la contraseña
+  password: process.env.DB_PASSWORD || 'vEDebKsTQxnXjqYsAYIsGrdvxQLclNAa', // Reemplaza <tu-password> por la contraseña
   database: process.env.DB_NAME || 'railway', // Nombre de la base de datos
   port: process.env.DB_PORT || 53728, // Puerto de la base de datos
 });
